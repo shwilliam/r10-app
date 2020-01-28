ab2ee155e7b5386492f9dfab5202b755f60fd124
William L <w-lindvall@outlook.com>
2020-01-28 14:51:38 -0800

    rotate expanding text icon on press

--- js/components/ExpandingText.js
+++ js/components/ExpandingText.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
   View,
   TouchableOpacity,
@@ -10,6 +10,7 @@ import {useToggle} from '../hooks'
 import {Subtitle, Section, Text} from './index'
 import styles from './ExpandingText.styles'
 import THEME from '../theme'
+import {Easing} from 'react-native-reanimated'
 
 const ExpandingTextContent = ({children}) => {
   LayoutAnimation.configureNext({
@@ -29,22 +30,53 @@ const ExpandingTextContent = ({children}) => {
   return <Animated.View>{children}</Animated.View>
 }
 
+const ExpandingTextToggle = ({open, onPress, children}) => {
+  const [rotation] = useState(new Animated.Value(0))
+
+  const spin = () => {
+    Animated.timing(rotation, {
+      toValue: open ? 0 : 1,
+      duration: 300,
+      easing: Easing.easeOut,
+    }).start()
+  }
+
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => {
+        spin()
+        onPress()
+      }}
+    >
+      <Animated.View
+        style={{
+          ...styles.iconContainer,
+          transform: [
+            {
+              rotate: rotation.interpolate({
+                inputRange: [0, 1],
+                outputRange: ['-90deg', '0deg'],
+              }),
+            },
+          ],
+        }}
+      >
+        <Subtitle variant="secondary">{open ? '-' : '+'}</Subtitle>
+      </Animated.View>
+      <Subtitle variant="secondary">{children}</Subtitle>
+    </TouchableOpacity>
+  )
+}
+
 const ExpandingText = ({label, children}) => {
   const [isOpen, toggleIsOpen] = useToggle(false)
 
   return (
     <View>
-      <TouchableOpacity
-        style={styles.container}
-        onPress={toggleIsOpen}
-      >
-        <View style={styles.iconContainer}>
-          <Subtitle variant="secondary">
-            {isOpen ? '-' : '+'}
-          </Subtitle>
-        </View>
-        <Subtitle variant="secondary">{label}</Subtitle>
-      </TouchableOpacity>
+      <ExpandingTextToggle open={isOpen} onPress={toggleIsOpen}>
+        {label}
+      </ExpandingTextToggle>
 
       <ExpandingTextContent>
         {isOpen && (
